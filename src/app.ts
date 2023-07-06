8b412bc30e9fbad12ca22dadca9d296b4cbbdb6f
Diogo-gallina <diogogallina20@gmail.com>
2023-07-06 17:09:26 -0300

    config: config-docker-compose

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    data: {
+        name: 'Diogo Gallina',
+        email: 'diogo@gmail.com'
+    }
+})
