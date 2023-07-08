276470e3c47c2eacc2c8698231fe90c907fe4814
Diogo-gallina <diogogallina20@gmail.com>
2023-07-08 15:40:31 -0300

    feat: add-factory-authenticate-use-casae

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,9 +1,8 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
 
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
-import { AuthenticateUseCase } from "@/use-cases/authenticate";
 import { UserAlreadyExistError } from "@/use-cases/errors/user-aleread-exists-error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 
 
 
@@ -17,8 +16,7 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
     const { email, password } = authenticateBodySchema.parse(request.body);
 
     try {
-        const usersRepository = new PrismaUsersRepository();
-        const authenticateUseCase = new AuthenticateUseCase(usersRepository);
+        const authenticateUseCase = makeAuthenticateUseCase();
 
         await authenticateUseCase.execute({
             email,
