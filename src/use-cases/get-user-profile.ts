a921c6a3c53ab047cd9c0c192e56742ffb0e52cb
Diogo-gallina <diogogallina20@gmail.com>
2023-07-10 10:50:59 -0300

    feat: cretae-getUserProfile-use-case

--- src/use-cases/get-user-profile.ts
+++ src/use-cases/get-user-profile.ts
@@ -2,7 +2,7 @@ import { compare } from "bcryptjs";
 import { User } from "@prisma/client";
 
 import { UsersRepository } from "@/repositories/users-repository";
-import { InvalidCredentialError } from "./errors/invalid-credentials-error";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface GetUserProfileUseCaseRequest {
     userId: string;
@@ -20,16 +20,10 @@ export class GetUserProfileUseCase {
     async execute({
         userId
     }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
-        const user = await this.usersRepository.findByEmail(email);
+        const user = await this.usersRepository.findById(userId);
 
         if (!user) {
-            throw new InvalidCredentialError();
-        }
-
-        const doesPasswordMatches = await compare(password, user.password_hash);
-
-        if (!doesPasswordMatches){
-            throw new InvalidCredentialError();
+            throw new ResourceNotFoundError();
         }
 
         return {
